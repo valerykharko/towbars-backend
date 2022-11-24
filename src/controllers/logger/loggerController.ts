ca23323f4c1d4dba5657ae8e448753355fd23c12
valerykharko <x-valery@mail.ru>
2022-11-24 21:48:31 +0300

    server: refactoring

--- src/controllers/logger/loggerController.ts
+++ src/controllers/logger/loggerController.ts
@@ -26,7 +26,7 @@ export default class TowbarController {
       const { type, payload, location } = req.body;
 
       let userId;
-      if (req.headers.authorization.split(" ")[1] !== "null") {
+      if (req.headers.authorization) {
         const token = req.headers.authorization.split(" ")[1];
         const { id } = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
         userId = id;
